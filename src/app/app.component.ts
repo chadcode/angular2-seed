d79f0e551691f5a99540808dd7f1ac6fa825f291
chad chad <chadtesting@gmail.com>
2016-12-02 12:38:54 -0600

    all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-<<<<<<< HEAD
-
-  selector   : 'app',
-=======
   selector: 'app',
->>>>>>> upstream/master
   templateUrl: './app.component.html',
 })
 export class AppComponent {
